619b1956917960b76d251fe75ff2b80acd6d9e94
Kyle Taylor <ktaylora@eo.uwyo.edu>
2014-08-28 22:16:36 -0600

    adding a quick bash log -> csv converter

--- cam-soilwat-unix-wrapper.sh
+++ cam-soilwat-unix-wrapper.sh
@@ -2,3 +2,4 @@
 site=$1; rm -rf $site.log; 
 oTime=`{ time nohup R --no-save --vanilla --slave --args $site < cam-soilwat-unix-wrapper.R >> $site.log & } 2>&1 | grep real` 
 echo $oTime | awk '{ print "runtime: " $2 }' >> $site.log
+grep -v " -" $site.log | grep -v "runtime" >> $site.csv
