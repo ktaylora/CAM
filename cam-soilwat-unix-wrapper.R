e0a32e636638e58068a27a7de6ba060133fe460b
Kyle Taylor <ktaylora@users.noreply.github.com>
2015-01-10 12:52:52 -0700

    Update cam-soilwat-unix-wrapper.R

--- cam-soilwat-unix-wrapper.R
+++ cam-soilwat-unix-wrapper.R
@@ -1,4 +1,9 @@
 source("cheatgrass-abundance-model.R")
 argv <- commandArgs(trailingOnly=T)
-Rsw_loadClimateDb("dbWeatherData_GTD.sqlite3")
-Rsw_CAM_run(sites=argv, Scenario="Current", years=c(1995,2001), debug=T, greppable=T)
+
+Rsw_setConnection(argv[2])
+o <- dbW_getWeatherData(Site_id=1);
+  years <- c(o[[1]]@year, o[[length(o)]]@year);
+    rm(o);
+    
+Rsw_CAM_run(sites=argv[1], Scenario="Current", years=years, debug=T, greppable=T)
