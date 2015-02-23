15f7bde90179ec423c94f8c3733482fd7bc2eff8
Kyle Taylor <ktaylora@users.noreply.github.com>
2015-02-23 05:14:20 -0700

    Update calculate_cam_site_statistics_for_raster_grids.R
    bug fix for handling sites with zero population growth due to unsuitable conditions.

--- calculate_cam_site_statistics_for_raster_grids.R
+++ calculate_cam_site_statistics_for_raster_grids.R
@@ -33,132 +33,149 @@ for(f in files){
   
   site <- as.numeric(gsub("([0-9]+).*$", "\\1", f)) # GREP out the site_id from the CSV filename
   
-  # calculate the site mean over time period and normalized variance for population size and seedbank size [~dispersion index] (mean/sd) 
-  t_years <- split(t, f=t$year)
-  t_years_ordered <- order(as.numeric(names(t_years)))
-  
-  # months of the year definitions
-  winter <- c(12,13,1,2)
-  spring <- c(3,4,5)
-  summer <- c(6,7,8)
-    fall <- c(9,10,11) 
-
-
-  monthly_aggregated_winterPopSize <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_springPopSize <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_summerPopSize <- rep(NA,length(t_years_ordered))
-    monthly_aggregated_fallPopSize <- rep(NA,length(t_years_ordered))
-
-  monthly_aggregated_winterBiomass <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_springBiomass <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_summerBiomass <- rep(NA,length(t_years_ordered))
-    monthly_aggregated_fallBiomass <- rep(NA,length(t_years_ordered))
-
-  monthly_aggregated_winterSeedbank <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_springSeedbank <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_summerSeedbank <- rep(NA,length(t_years_ordered))
-    monthly_aggregated_fallSeedbank <- rep(NA,length(t_years_ordered))
-
-  monthly_aggregated_winterRtLen <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_springRtLen <- rep(NA,length(t_years_ordered))
-  monthly_aggregated_summerRtLen <- rep(NA,length(t_years_ordered))
-    monthly_aggregated_fallRtLen <- rep(NA,length(t_years_ordered))
-  
-  # chunk each year into monthly intervals
-  mon_rootLen <- lapply(t_years, FUN=processChunks, var='p.mnRtLen', size=30)
-     mon_rootLen <- mon_rootLen[(names(mon_rootLen) != "NULL")]
-  mon_age <- lapply(t_years, FUN=processChunks, var='p.meanAge', size=30)
-     mon_age <- mon_rootLen[(names(mon_age) != "NULL")]
-  mon_pop_size <- lapply(t_years, FUN=processChunks, var='p.size', size=30)
-     mon_pop_size <- mon_pop_size[(names(mon_pop_size) != "NULL")]
-  mon_abg_bio  <- lapply(t_years, FUN=processChunks, var='ag.mdBiomass', size=30) 
-    mon_abg_bio <- mon_abg_bio[(names(mon_abg_bio) != "NULL")]
-  mon_sb_size  <- lapply(t_years, FUN=processChunks, var='sb.size', size=30) 
-    mon_sb_size <- mon_sb_size[(names(mon_sb_size) != "NULL")]
-
-  # iterate over each year in the aggregation
-  for(i in 1:length(mon_pop_size)){
-    # take the monthly mean across all months for year i
-    popSize <- round(unlist(lapply(mon_pop_size[[i]], FUN=mean, na.rm=T)))
-    abgBiomass <- unlist(lapply(mon_abg_bio[[i]], FUN=mean, na.rm=T))
-    seedbankSize <- round(unlist(lapply(mon_sb_size[[i]], FUN=mean, na.rm=T)))
-    rootLength <- round(unlist(lapply(mon_rootLen[[i]], FUN=mean, na.rm=T)),2)
-
-    # aggregate our monthly means by season
-    monthly_aggregated_winterPopSize[i] <- round(mean(popSize[winter],na.rm=T))
-    monthly_aggregated_springPopSize[i] <- round(mean(popSize[spring], na.rm=T))
-    monthly_aggregated_summerPopSize[i] <- round(mean(popSize[summer], na.rm=T))
-    monthly_aggregated_fallPopSize[i]   <- round(mean(popSize[fall], na.rm=T))
-
-    monthly_aggregated_winterBiomass[i] <- round(mean(popSize[winter],na.rm=T))*mean(abgBiomass[winter],na.rm=T)
-    monthly_aggregated_springBiomass[i] <- round(mean(popSize[spring], na.rm=T))*mean(abgBiomass[spring],na.rm=T)
-    monthly_aggregated_summerBiomass[i] <- round(mean(popSize[summer], na.rm=T))*mean(abgBiomass[summer],na.rm=T)
-    monthly_aggregated_fallBiomass[i]   <- round(mean(popSize[fall], na.rm=T))*mean(abgBiomass[fall],na.rm=T)
-    
-    monthly_aggregated_winterSeedbank[i] <- round(mean(seedbankSize[winter],na.rm=T))
-    monthly_aggregated_springSeedbank[i] <- round(mean(seedbankSize[spring],na.rm=T))
-    monthly_aggregated_summerSeedbank[i] <- round(mean(seedbankSize[summer],na.rm=T))
-    monthly_aggregated_fallSeedbank[i]   <- round(mean(seedbankSize[fall],na.rm=T))
-    
-    monthly_aggregated_winterRtLen[i] <- round(mean(rootLength[winter],na.rm=T))
-    monthly_aggregated_springRtLen[i] <- round(mean(rootLength[spring],na.rm=T))
-    monthly_aggregated_summerRtLen[i] <- round(mean(rootLength[summer],na.rm=T))
-    monthly_aggregated_fallRtLen[i]   <- round(mean(rootLength[fall],na.rm=T))
-  }
-
-  # statistics to record to attribute table
-  pop_mn_mon <- round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*PopSize")), FUN=get)),na.rm=T),2)
-  pop_se_mon <- round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*PopSize")), FUN=get)),na.rm=T),2)
-  pop_mn_spr <- round(mean(monthly_aggregated_springPopSize, na.rm=T),2)
-  pop_mn_sum <- round(mean(monthly_aggregated_summerPopSize, na.rm=T),2)
-  pop_mn_win <- round(mean(monthly_aggregated_winterPopSize, na.rm=T),2)
-  pop_mn_fal <- round(mean(monthly_aggregated_fallPopSize, na.rm=T),2)
-  pop_sw_rat <- pop_mn_sum / pop_mn_win
-  pop_sf_rat <- pop_mn_spr / pop_mn_fal
-
-  abg_mn_mon <- round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*Biomass")), FUN=get)),na.rm=T),2)
-  abg_se_mon <- round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*Biomass")), FUN=get)),na.rm=T),2)
-  abg_mn_spr <- round(mean(monthly_aggregated_springBiomass, na.rm=T),2)
-  abg_mn_sum <- round(mean(monthly_aggregated_summerBiomass, na.rm=T),2)
-  abg_mn_win <- round(mean(monthly_aggregated_winterBiomass, na.rm=T),2)
-  abg_mn_fal <- round(mean(monthly_aggregated_fallBiomass, na.rm=T),2)
-  abg_sw_rat <- abg_mn_sum / abg_mn_win
-  abg_sf_rat <- abg_mn_spr / abg_mn_fal
-
-  sb_mn_mon <-  round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*Seedbank")), FUN=get)),na.rm=T),2)
-  sb_se_mon <-  round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*Seedbank")), FUN=get)),na.rm=T),2)
-  sb_mn_spr <-  round(mean(monthly_aggregated_springSeedbank, na.rm=T),2)
-  sb_mn_sum <-  round(mean(monthly_aggregated_summerSeedbank, na.rm=T),2)
-  sb_mn_win <-  round(mean(monthly_aggregated_winterSeedbank, na.rm=T),2)
-  sb_mn_fal <-  round(mean(monthly_aggregated_fallSeedbank, na.rm=T),2)
-  sb_sw_rat <-  sb_mn_sum / sb_mn_win
-  sb_sf_rat <-  sb_mn_spr / sb_mn_fal
-
-  rl_mn_mon <-  round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*RtLen")), FUN=get)),na.rm=T),2)
-  rl_se_mon <-  round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*RtLen")), FUN=get)),na.rm=T),2)
-  rl_mn_spr <-  round(mean(monthly_aggregated_springRtLen, na.rm=T),2)
-  rl_mn_sum <-  round(mean(monthly_aggregated_summerRtLen, na.rm=T),2)
-  rl_mn_win <-  round(mean(monthly_aggregated_winterRtLen, na.rm=T),2)
-  rl_mn_fal <-  round(mean(monthly_aggregated_fallRtLen, na.rm=T),2)
-  rl_sw_rat <-  rl_mn_sum / rl_mn_win
-  rl_sf_rat <-  rl_mn_spr / rl_mn_fal
-
-  persistence <- nrow(t) # number of days across simulation the population survived (indicates population crashes)
-
-  focal<-data.frame(site_id=site,abg_mn_mon=abg_mn_mon,abg_se_mon=abg_se_mon,abg_mn_spr=abg_mn_spr,abg_mn_sum=abg_mn_sum,abg_mn_win=abg_mn_win,abg_mn_fal=abg_mn_fal,
-                    sb_mn_mon=sb_mn_mon,sb_se_mon=sb_se_mon,sb_mn_spr=sb_mn_spr,sb_mn_sum=sb_mn_sum,sb_mn_win=sb_mn_win,sb_mn_fal=sb_mn_fal,
-                    rl_mn_mon= rl_mn_mon,rl_se_mon=rl_se_mon,rl_mn_spr=rl_mn_spr,rl_mn_sum=rl_mn_sum,rl_mn_win=rl_mn_win,rl_mn_fal=rl_mn_fal,
-                    pop_mn_mon=pop_mn_mon,pop_se_mon=pop_se_mon,pop_mn_spr=pop_mn_spr,pop_mn_sum=pop_mn_sum,pop_mn_win=pop_mn_win,pop_mn_fal=pop_mn_fal,
-                    pop_sw_rat=pop_sw_rat,pop_sf_rat=pop_sf_rat,abg_sw_rat=abg_sw_rat,abg_sf_rat=abg_sf_rat,sb_sw_rat=sb_sw_rat,sb_sf_rat=sb_sf_rat,
-                    rl_sw_rat=rl_sw_rat,rl_sf_rat=rl_sf_rat,
-                    pers=persistence)
-  
-  if(nrow(out)>0){
-    out <- rbind(out,focal)
+  # bug fix for sites that experience no germination due to unsuitable conditions
+  if(nrow(t) == 0){
+      focal<-data.frame(site_id=site,abg_mn_mon=0,abg_se_mon=0,abg_mn_spr=0,abg_mn_sum=0,abg_mn_win=0,abg_mn_fal=0,
+                        sb_mn_mon=0,sb_se_mon=0,sb_mn_spr=0,sb_mn_sum=0,sb_mn_win=0,sb_mn_fal=0,
+                        rl_mn_mon=0,rl_se_mon=0,rl_mn_spr=0,rl_mn_sum=0,rl_mn_win=0,rl_mn_fal=0,
+                        pop_mn_mon=0,pop_se_mon=0,pop_mn_spr=0,pop_mn_sum=0,pop_mn_win=0,pop_mn_fal=0,
+                        pop_sw_rat=0,pop_sf_rat=0,abg_sw_rat=0,abg_sf_rat=0,sb_sw_rat=0,sb_sf_rat=0,
+                        rl_sw_rat=0,rl_sf_rat=0,
+                        pers=0)
+      
+      if(nrow(out)>0){
+        out <- rbind(out,focal)
+      } else {
+        out <- focal
+      }
   } else {
-    out <- focal
-  }
-  cat(".");
+     # calculate the site mean over time period and normalized variance for population size and seedbank size [~dispersion index] (mean/sd) 
+      t_years <- split(t, f=t$year)
+      t_years_ordered <- order(as.numeric(names(t_years)))
+      
+      # months of the year definitions
+      winter <- c(12,13,1,2)
+      spring <- c(3,4,5)
+      summer <- c(6,7,8)
+        fall <- c(9,10,11) 
+
+
+      monthly_aggregated_winterPopSize <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_springPopSize <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_summerPopSize <- rep(NA,length(t_years_ordered))
+        monthly_aggregated_fallPopSize <- rep(NA,length(t_years_ordered))
+
+      monthly_aggregated_winterBiomass <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_springBiomass <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_summerBiomass <- rep(NA,length(t_years_ordered))
+        monthly_aggregated_fallBiomass <- rep(NA,length(t_years_ordered))
+
+      monthly_aggregated_winterSeedbank <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_springSeedbank <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_summerSeedbank <- rep(NA,length(t_years_ordered))
+        monthly_aggregated_fallSeedbank <- rep(NA,length(t_years_ordered))
+
+      monthly_aggregated_winterRtLen <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_springRtLen <- rep(NA,length(t_years_ordered))
+      monthly_aggregated_summerRtLen <- rep(NA,length(t_years_ordered))
+        monthly_aggregated_fallRtLen <- rep(NA,length(t_years_ordered))
+      
+      # chunk each year into monthly intervals
+      mon_rootLen <- lapply(t_years, FUN=processChunks, var='p.mnRtLen', size=30)
+         mon_rootLen <- mon_rootLen[(names(mon_rootLen) != "NULL")]
+      mon_age <- lapply(t_years, FUN=processChunks, var='p.meanAge', size=30)
+         mon_age <- mon_rootLen[(names(mon_age) != "NULL")]
+      mon_pop_size <- lapply(t_years, FUN=processChunks, var='p.size', size=30)
+         mon_pop_size <- mon_pop_size[(names(mon_pop_size) != "NULL")]
+      mon_abg_bio  <- lapply(t_years, FUN=processChunks, var='ag.mdBiomass', size=30) 
+        mon_abg_bio <- mon_abg_bio[(names(mon_abg_bio) != "NULL")]
+      mon_sb_size  <- lapply(t_years, FUN=processChunks, var='sb.size', size=30) 
+        mon_sb_size <- mon_sb_size[(names(mon_sb_size) != "NULL")]
+
+      # iterate over each year in the aggregation
+      for(i in 1:length(mon_pop_size)){ 
+        # take the monthly mean across all months for year i
+        popSize <- round(unlist(lapply(mon_pop_size[[i]], FUN=mean, na.rm=T)))
+        abgBiomass <- unlist(lapply(mon_abg_bio[[i]], FUN=mean, na.rm=T))
+        seedbankSize <- round(unlist(lapply(mon_sb_size[[i]], FUN=mean, na.rm=T)))
+        rootLength <- round(unlist(lapply(mon_rootLen[[i]], FUN=mean, na.rm=T)),2)
+        rootLength <- round(unlist(lapply(mon_rootLen[[i]], FUN=mean, na.rm=T)),2)
+
+        # aggregate our monthly means by season
+        monthly_aggregated_winterPopSize[i] <- round(mean(popSize[winter],na.rm=T))
+        monthly_aggregated_springPopSize[i] <- round(mean(popSize[spring], na.rm=T))
+        monthly_aggregated_summerPopSize[i] <- round(mean(popSize[summer], na.rm=T))
+        monthly_aggregated_fallPopSize[i]   <- round(mean(popSize[fall], na.rm=T))
+
+        monthly_aggregated_winterBiomass[i] <- round(mean(popSize[winter],na.rm=T))*mean(abgBiomass[winter],na.rm=T)
+        monthly_aggregated_springBiomass[i] <- round(mean(popSize[spring], na.rm=T))*mean(abgBiomass[spring],na.rm=T)
+        monthly_aggregated_summerBiomass[i] <- round(mean(popSize[summer], na.rm=T))*mean(abgBiomass[summer],na.rm=T)
+        monthly_aggregated_fallBiomass[i]   <- round(mean(popSize[fall], na.rm=T))*mean(abgBiomass[fall],na.rm=T)
+        
+        monthly_aggregated_winterSeedbank[i] <- round(mean(seedbankSize[winter],na.rm=T))
+        monthly_aggregated_springSeedbank[i] <- round(mean(seedbankSize[spring],na.rm=T))
+        monthly_aggregated_summerSeedbank[i] <- round(mean(seedbankSize[summer],na.rm=T))
+        monthly_aggregated_fallSeedbank[i]   <- round(mean(seedbankSize[fall],na.rm=T))
+        
+        monthly_aggregated_winterRtLen[i] <- round(mean(rootLength[winter],na.rm=T))
+        monthly_aggregated_springRtLen[i] <- round(mean(rootLength[spring],na.rm=T))
+        monthly_aggregated_summerRtLen[i] <- round(mean(rootLength[summer],na.rm=T))
+        monthly_aggregated_fallRtLen[i]   <- round(mean(rootLength[fall],na.rm=T))
+      }
+
+      # statistics to record to attribute table
+      pop_mn_mon <- round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*PopSize")), FUN=get)),na.rm=T),2)
+      pop_se_mon <- round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*PopSize")), FUN=get)),na.rm=T),2)
+      pop_mn_spr <- round(mean(monthly_aggregated_springPopSize, na.rm=T),2)
+      pop_mn_sum <- round(mean(monthly_aggregated_summerPopSize, na.rm=T),2)
+      pop_mn_win <- round(mean(monthly_aggregated_winterPopSize, na.rm=T),2)
+      pop_mn_fal <- round(mean(monthly_aggregated_fallPopSize, na.rm=T),2)
+      pop_sw_rat <- pop_mn_sum / pop_mn_win
+      pop_sf_rat <- pop_mn_spr / pop_mn_fal
+
+      abg_mn_mon <- round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*Biomass")), FUN=get)),na.rm=T),2)
+      abg_se_mon <- round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*Biomass")), FUN=get)),na.rm=T),2)
+      abg_mn_spr <- round(mean(monthly_aggregated_springBiomass, na.rm=T),2)
+      abg_mn_sum <- round(mean(monthly_aggregated_summerBiomass, na.rm=T),2)
+      abg_mn_win <- round(mean(monthly_aggregated_winterBiomass, na.rm=T),2)
+      abg_mn_fal <- round(mean(monthly_aggregated_fallBiomass, na.rm=T),2)
+      abg_sw_rat <- abg_mn_sum / abg_mn_win
+      abg_sf_rat <- abg_mn_spr / abg_mn_fal
+
+      sb_mn_mon <-  round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*Seedbank")), FUN=get)),na.rm=T),2)
+      sb_se_mon <-  round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*Seedbank")), FUN=get)),na.rm=T),2)
+      sb_mn_spr <-  round(mean(monthly_aggregated_springSeedbank, na.rm=T),2)
+      sb_mn_sum <-  round(mean(monthly_aggregated_summerSeedbank, na.rm=T),2)
+      sb_mn_win <-  round(mean(monthly_aggregated_winterSeedbank, na.rm=T),2)
+      sb_mn_fal <-  round(mean(monthly_aggregated_fallSeedbank, na.rm=T),2)
+      sb_sw_rat <-  sb_mn_sum / sb_mn_win
+      sb_sf_rat <-  sb_mn_spr / sb_mn_fal
+
+      rl_mn_mon <-  round(mean(unlist(lapply(ls(pattern=glob2rx("monthly_*RtLen")), FUN=get)),na.rm=T),2)
+      rl_se_mon <-  round(sd(unlist(lapply(ls(pattern=glob2rx("monthly_*RtLen")), FUN=get)),na.rm=T),2)
+      rl_mn_spr <-  round(mean(monthly_aggregated_springRtLen, na.rm=T),2)
+      rl_mn_sum <-  round(mean(monthly_aggregated_summerRtLen, na.rm=T),2)
+      rl_mn_win <-  round(mean(monthly_aggregated_winterRtLen, na.rm=T),2)
+      rl_mn_fal <-  round(mean(monthly_aggregated_fallRtLen, na.rm=T),2)
+      rl_sw_rat <-  rl_mn_sum / rl_mn_win
+      rl_sf_rat <-  rl_mn_spr / rl_mn_fal
+
+      persistence <- nrow(t) # number of days across simulation the population survived (indicates population crashes)
+
+      focal<-data.frame(site_id=site,abg_mn_mon=abg_mn_mon,abg_se_mon=abg_se_mon,abg_mn_spr=abg_mn_spr,abg_mn_sum=abg_mn_sum,abg_mn_win=abg_mn_win,abg_mn_fal=abg_mn_fal,
+                        sb_mn_mon=sb_mn_mon,sb_se_mon=sb_se_mon,sb_mn_spr=sb_mn_spr,sb_mn_sum=sb_mn_sum,sb_mn_win=sb_mn_win,sb_mn_fal=sb_mn_fal,
+                        rl_mn_mon= rl_mn_mon,rl_se_mon=rl_se_mon,rl_mn_spr=rl_mn_spr,rl_mn_sum=rl_mn_sum,rl_mn_win=rl_mn_win,rl_mn_fal=rl_mn_fal,
+                        pop_mn_mon=pop_mn_mon,pop_se_mon=pop_se_mon,pop_mn_spr=pop_mn_spr,pop_mn_sum=pop_mn_sum,pop_mn_win=pop_mn_win,pop_mn_fal=pop_mn_fal,
+                        pop_sw_rat=pop_sw_rat,pop_sf_rat=pop_sf_rat,abg_sw_rat=abg_sw_rat,abg_sf_rat=abg_sf_rat,sb_sw_rat=sb_sw_rat,sb_sf_rat=sb_sf_rat,
+                        rl_sw_rat=rl_sw_rat,rl_sf_rat=rl_sf_rat,
+                        pers=persistence)
+      
+      if(nrow(out)>0){
+        out <- rbind(out,focal)
+      } else {
+        out <- focal
+      }
+  };cat(".");
 }; cat("\n");
 
 cat(" -- writing to processed.pop.metrics.csv.\n")
